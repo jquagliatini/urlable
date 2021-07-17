ef5979cd3fb38612a4054361addec38096c0a002
Jordan Quagliatini <contact@jquagliatini.fr>
2021-07-17 21:24:56 +0200

    feat: adds more use cas tests

--- lib/__tests__/urlable.spec.ts
+++ lib/__tests__/urlable.spec.ts
@@ -2,20 +2,20 @@ import { defaultSerialize, queryStringReconciliation, urlable, urlAsObject } fro
 import { mocked } from 'ts-jest/utils';
 
 describe('urlAsObject', () => {
-	it.each`
-		input                                        | expected
-		${'http://local.host?hello=world'}           | ${{ hello: 'world' }}
-		${'http://local.host?p=12&q=Something+else'} | ${{ p: '12', q: 'Something else' }}
+  it.each`
+    input                                        | expected
+    ${'http://local.host?hello=world'}           | ${{ hello: 'world' }}
+    ${'http://local.host?p=12&q=Something+else'} | ${{ p: '12', q: 'Something else' }}
     ${'http://local.host?p=1'}                   | ${{ p: '1' }}
-	`('should parse $input as $expected', ({ input, expected }) => {
-		expect(urlAsObject(input)).toStrictEqual(expected);
-	});
+  `('should parse $input as $expected', ({ input, expected }) => {
+    expect(urlAsObject(input)).toStrictEqual(expected);
+  });
 });
 
 describe('queryStringReconciliation', () => {
   it('should return { shouldWrite: false } when no arg is provided', () => {
     const { shouldWrite, queryString } = queryStringReconciliation({
-      readUrl: () => 'http://localhost?hello=world'
+      readUrl: () => 'http://localhost?hello=world',
     });
     expect(shouldWrite).toBe(false);
     expect(queryString).toBe('?hello=world');
@@ -24,7 +24,7 @@ describe('queryStringReconciliation', () => {
   it('should add new properties to existing query string', () => {
     const { shouldWrite, queryString } = queryStringReconciliation({
       readUrl: () => 'http://localhost?hello=world',
-      newQueryString: { p: '1', pageSize: '12' }
+      newQueryString: { p: '1', pageSize: '12' },
     });
 
     expect(shouldWrite).toBe(true);
@@ -34,7 +34,7 @@ describe('queryStringReconciliation', () => {
   it('should update the existing key', () => {
     const { queryString } = queryStringReconciliation({
       readUrl: () => 'http://localhost?hello=world',
-      newQueryString: { hello: 'John' }
+      newQueryString: { hello: 'John' },
     });
 
     expect(queryString).toBe(`?hello=John`);
@@ -43,7 +43,7 @@ describe('queryStringReconciliation', () => {
   it('should remove, when providing en empty value', () => {
     const { queryString } = queryStringReconciliation({
       readUrl: () => 'http://localhost?hello=world',
-      newQueryString: { hello: '' }
+      newQueryString: { hello: '' },
     });
 
     expect(queryString).toBe('');
@@ -52,7 +52,7 @@ describe('queryStringReconciliation', () => {
   it('should return `{ shouldWrite: false }`, when the query does not change', () => {
     const { shouldWrite, queryString } = queryStringReconciliation({
       readUrl: () => 'http://localhost?hello=world',
-      newQueryString: { hello: 'world' }
+      newQueryString: { hello: 'world' },
     });
 
     expect(shouldWrite).toBe(false);
@@ -64,7 +64,7 @@ describe('defaultSerialize', () => {
   it.each`
     input                               | expected
     ${{ p: 1, pageSize: 12 }}           | ${{ p: '1', pageSize: '12' }}
-    ${{ selected: [1, 2, 3] }}          | ${{selected: '1,2,3' }}
+    ${{ selected: [1, 2, 3] }}          | ${{ selected: '1,2,3' }}
     ${{ selected: [] }}                 | ${{}}
     ${{ color: 'RED' }}                 | ${{ color: 'RED' }}
     ${{ color: '' }}                    | ${{}}
@@ -78,41 +78,109 @@ describe('defaultSerialize', () => {
 });
 
 describe('urlable', () => {
-	let writeUrl = jest.fn();
+  let writeUrl = jest.fn();
+  const baseUrl = 'http://localhost';
+  let url = baseUrl;
 
-	beforeEach(() => {
-		mocked(writeUrl).mockClear();
-	});
-
-	it('should update the queryString on first call', () => {
-		urlable(
-			{
-				page: 1,
-				pageSize: 12,
-			},
-			{
-				writeUrl,
-				readUrl: () => 'http://localhost/?hello=world',
-			},
-		);
-
-		expect(writeUrl).toHaveBeenCalledWith(`?hello=world&page=1&pageSize=12`);
-	});
-
-  it('should update the query string, when the observed object changes', () => {
-    const baseUrl = 'http://localhost';
-    let url = baseUrl;
+  beforeEach(() => {
+    url = baseUrl;
+    mocked(writeUrl).mockClear();
     mocked(writeUrl).mockImplementation((queryString: string) => {
       url = baseUrl + queryString;
     });
+  });
 
-    const pagination = urlable({
-      page: 1,
-      pageSize: 12
-    }, { writeUrl, readUrl: () => url });
+  it('should update the queryString on first call', () => {
+    urlable(
+      {
+        page: 1,
+        pageSize: 12,
+      },
+      {
+        writeUrl,
+        readUrl: () => 'http://localhost/?hello=world',
+      },
+    );
+
+    expect(writeUrl).toHaveBeenCalledWith(`?hello=world&page=1&pageSize=12`);
+  });
+
+  it('should update the query string, when the observed object changes', () => {
+    const pagination = urlable(
+      {
+        page: 1,
+        pageSize: 12,
+      },
+      { writeUrl, readUrl: () => url },
+    );
 
     pagination.page = 2;
 
     expect(writeUrl).toHaveBeenNthCalledWith(2, '?page=2&pageSize=12');
+  });
+
+  it('should hide a property if hidden in serialize', () => {
+    const pagination = urlable(
+      {
+        page: 1,
+        totalPages: 10,
+      },
+      {
+        writeUrl,
+        readUrl: () => url,
+        serialize: (x) => ({ page: String(x.page) }),
+      },
+    );
+
+    pagination.totalPages = 11;
+    pagination.page = 2;
+
+    expect(writeUrl).toHaveBeenCalledTimes(2);
+    expect(writeUrl).toHaveBeenNthCalledWith(1, '?page=1');
+    expect(writeUrl).toHaveBeenNthCalledWith(2, '?page=2');
+  });
+
+  it('should rename a property', () => {
+    urlable(
+      {
+        currentPage: 1,
+      },
+      {
+        writeUrl,
+        readUrl: () => url,
+        serialize: (x) => ({ page: String(x.currentPage) }),
+      },
+    );
+
+    expect(writeUrl).toHaveBeenCalledWith(`?page=1`);
+  });
+
+  it('should initialize the state, with the URL, and the correct type', () => {
+    interface User {
+      name: string;
+      age: number;
+    }
+
+    url = 'http://localhost?name=Georges&age=32';
+
+    const user = urlable(
+      {
+        name: '',
+        age: -1,
+      },
+      {
+        writeUrl,
+        readUrl: () => url,
+        deserialize: (x) => ({
+          name: x?.name ?? '',
+          age: Number(x?.age ?? -1),
+        }),
+      },
+    );
+
+    expect(writeUrl).not.toHaveBeenCalled();
+
+    expect(user.name).toBe('Georges');
+    expect(user.age).toBe(32);
   });
 });
